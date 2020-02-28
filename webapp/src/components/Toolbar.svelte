8f34dbbec6ed112b2c3a0f7cbed91bf371ed8a74
Marcin J <jerzakm@gmail.com>
2020-02-28 14:02:08 +0100

    barebones google auth on topbar

--- webapp/src/components/Toolbar.svelte
+++ webapp/src/components/Toolbar.svelte
@@ -3,6 +3,7 @@
     import Button, {Label} from '@smui/button';
     import IconButton, {Icon} from '@smui/icon-button';
     import List, {Item, Text} from '@smui/list';
+    import Ripple from '@smui/ripple';
 </script>
 
 <style>
@@ -10,17 +11,21 @@
         position: fixed;
         top: 50px;
         left: 50px;
+        user-select: none;
+    }
+    .toolbar-button{
+        cursor: pointer;
+        padding: 5px;
+    }
+    .inactive {
+        opacity: 0.9;
     }
 </style>
 
 <div class="card-container toolbar">
     <div>
         <Card>
-            <Content component={List}>
-                <Item>
-                    <Icon class="material-icons" on>grid_on</Icon>
-                </Item>
-            </Content>
+            <div use:Ripple={{ripple: true, unbounded: true, color: 'primary'}} class="toolbar-button inactive"><Icon class="material-icons">grid_on</Icon></div>
         </Card>
     </div>
 </div>
---
+++ webapp/src/components/Topbar.svelte
@@ -0,0 +1,94 @@
+<script>
+    import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
+    // import IconButton from '@smui/icon-button';
+    import Button, {Label} from '@smui/button'
+
+    import Menu, {SelectionGroup, SelectionGroupIcon} from '@smui/menu';
+    import {Anchor} from '@smui/menu-surface';
+    import List, {Item, Separator, Text, PrimaryText, SecondaryText, Graphic} from '@smui/list';
+
+    // Firebase App (the core Firebase SDK) is always required and
+    // must be listed before other Firebase SDKs
+    import firebase from "firebase/app";
+
+    // Add the Firebase services that you want to use
+    import "firebase/auth";
+    import "firebase/firestore";
+
+    let menu
+    let clicked
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyAjCgYB9VdcCacpWLaWTN4UBCXoUgJlHGA",
+        authDomain: "pixelizerwebapp.firebaseapp.com",
+        databaseURL: "https://pixelizerwebapp.firebaseio.com",
+        projectId: "pixelizerwebapp",
+        storageBucket: "pixelizerwebapp.appspot.com",
+        messagingSenderId: "464760066182",
+        appId: "1:464760066182:web:2f0462df33feaf7d81c107",
+        measurementId: "G-G2QQYDS1BB"
+    };
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    let user
+
+    const handleGoogleLogin = () => {
+        auth
+        .signInWithPopup(provider)
+        .then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const token = result.credential.accessToken;
+            const firebaseuser = result.user;
+            userAuth.set(result);
+        })
+        .catch(function(error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.email;
+            const credential = error.credential;
+            userAuth.set(undefined);
+        });
+    };
+
+    const handleSignOut = () => {
+        auth.signOut()
+        userAuth.set(undefined);
+    }
+
+    // handleGoogleLogin()
+
+    import { userAuth } from '../stores.ts';
+
+
+    const unsubscribe = userAuth.subscribe(value => {
+        console.log(`subbed value changed`, value)
+        user = value
+    });
+
+    let anchor
+</script>
+
+<style>
+
+</style>
+
+
+<TopAppBar variant="static" >
+    <Row>
+        <Section>
+            <Title>webapp</Title>
+        </Section>
+        <Section align="end" toolbar>
+            <div>
+                {#if user}
+                    <Button on:click={() => handleSignOut()} variant="raised" color="secondary" class="button-shaped-round"><Label>Sign out</Label></Button>
+                {:else}
+                    <Button on:click={() => handleGoogleLogin()} variant="raised" color="secondary" class="button-shaped-round"><Label>Sign in</Label></Button>
+                {/if}
+            </div>
+        </Section>
+    </Row>
+</TopAppBar>
+
