498223bd6595e29557458ef16d766503c56001ce
Marcin <jerzakm@gmail.com>
2020-02-29 19:34:57 +0100

    firebase checks auth after page refresh

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,22 +1,5 @@
-<script lang="ts">
-    // import Layers from './components/Layers.svelte'
-    import Toolbar from './components/Toolbar.svelte'
-    import Topbar from './components/Topbar.svelte'
-    // import {initPixiCanvas} from './canvas/renderer'
-    // import {Graphics, Sprite}     from 'pixi.js'
-
-    // const {renderer, ticker, stage, canvas} = initPixiCanvas()
-
-
-
-    // const sprite = Sprite.from('test.jpg')
-    // sprite.scale.set(0.25, 0.25)
-    // stage.addChild(sprite)
-
-    // ticker.add(()=> {
-    // })
-
+<script>
+  import Topbar from "./components/Topbar.svelte";
 </script>
 
-<Topbar/>
-<!-- <Toolbar/> -->
+<Topbar />
--- webapp/src/components/Topbar.svelte
+++ webapp/src/components/Topbar.svelte
@@ -1,94 +1,114 @@
 <script>
-    import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
-    // import IconButton from '@smui/icon-button';
-    import Button, {Label} from '@smui/button'
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  // import IconButton from '@smui/icon-button';
+  import Button, { Label } from "@smui/button";
 
-    import Menu, {SelectionGroup, SelectionGroupIcon} from '@smui/menu';
-    import {Anchor} from '@smui/menu-surface';
-    import List, {Item, Separator, Text, PrimaryText, SecondaryText, Graphic} from '@smui/list';
+  import Menu, { SelectionGroup, SelectionGroupIcon } from "@smui/menu";
+  import { Anchor } from "@smui/menu-surface";
+  import List, {
+    Item,
+    Separator,
+    Text,
+    PrimaryText,
+    SecondaryText,
+    Graphic
+  } from "@smui/list";
 
-    // Firebase App (the core Firebase SDK) is always required and
-    // must be listed before other Firebase SDKs
-    import firebase from "firebase/app";
+  // Firebase App (the core Firebase SDK) is always required and
+  // must be listed before other Firebase SDKs
+  import firebase from "firebase/app";
 
-    // Add the Firebase services that you want to use
-    import "firebase/auth";
-    import "firebase/firestore";
+  // Add the Firebase services that you want to use
+  import "firebase/auth";
+  import "firebase/firestore";
 
-    let menu
-    let clicked
+  let menu;
+  let clicked;
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyAjCgYB9VdcCacpWLaWTN4UBCXoUgJlHGA",
-        authDomain: "pixelizerwebapp.firebaseapp.com",
-        databaseURL: "https://pixelizerwebapp.firebaseio.com",
-        projectId: "pixelizerwebapp",
-        storageBucket: "pixelizerwebapp.appspot.com",
-        messagingSenderId: "464760066182",
-        appId: "1:464760066182:web:2f0462df33feaf7d81c107",
-        measurementId: "G-G2QQYDS1BB"
-    };
-    firebase.initializeApp(firebaseConfig);
-    const auth = firebase.auth();
-    const provider = new firebase.auth.GoogleAuthProvider();
+  const firebaseConfig = {
+    apiKey: "AIzaSyAjCgYB9VdcCacpWLaWTN4UBCXoUgJlHGA",
+    authDomain: "pixelizerwebapp.firebaseapp.com",
+    databaseURL: "https://pixelizerwebapp.firebaseio.com",
+    projectId: "pixelizerwebapp",
+    storageBucket: "pixelizerwebapp.appspot.com",
+    messagingSenderId: "464760066182",
+    appId: "1:464760066182:web:2f0462df33feaf7d81c107",
+    measurementId: "G-G2QQYDS1BB"
+  };
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
 
-    let user
+  let user;
 
-    const handleGoogleLogin = () => {
-        auth
-        .signInWithPopup(provider)
-        .then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const token = result.credential.accessToken;
-            const firebaseuser = result.user;
-            userAuth.set(result);
-        })
-        .catch(function(error) {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.email;
-            const credential = error.credential;
-            userAuth.set(undefined);
-        });
-    };
-
-    const handleSignOut = () => {
-        auth.signOut()
+  const handleGoogleLogin = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const token = result.credential.accessToken;
+        const firebaseuser = result.user;
+        userAuth.set(result);
+      })
+      .catch(function(error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
         userAuth.set(undefined);
+      });
+  };
+
+  const handleSignOut = () => {
+    auth.signOut();
+    userAuth.set(undefined);
+  };
+
+  // handleGoogleLogin()
+
+  import { userAuth } from "../stores.ts";
+
+  const unsubscribe = userAuth.subscribe(value => {
+    console.log(`subbed value changed`, value);
+    user = value;
+  });
+
+  firebase.auth().onAuthStateChanged(user => {
+    // If user exist (is logged in) --> store in state.
+    if (user) {
+      userAuth.set(user);
+    } else {
+      userAuth.set(undefined);
     }
-
-    // handleGoogleLogin()
-
-    import { userAuth } from '../stores.ts';
-
-
-    const unsubscribe = userAuth.subscribe(value => {
-        console.log(`subbed value changed`, value)
-        user = value
-    });
-
-    let anchor
+  });
 </script>
 
-<style>
-
-</style>
-
-
-<TopAppBar variant="static" >
-    <Row>
-        <Section>
-            <Title>webapp</Title>
-        </Section>
-        <Section align="end" toolbar>
-            <div>
-                {#if user}
-                    <Button on:click={() => handleSignOut()} variant="raised" color="secondary" class="button-shaped-round"><Label>Sign out</Label></Button>
-                {:else}
-                    <Button on:click={() => handleGoogleLogin()} variant="raised" color="secondary" class="button-shaped-round"><Label>Sign in</Label></Button>
-                {/if}
-            </div>
-        </Section>
-    </Row>
+<TopAppBar variant="static">
+  <Row>
+    <Section>
+      <Title>webapp</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <div>
+        {#if user}
+          <Label>{user.displayName}</Label>
+          <Button
+            on:click={() => handleSignOut()}
+            variant="raised"
+            color="secondary"
+            class="button-shaped-round">
+            <Label>Sign out</Label>
+          </Button>
+        {:else}
+          <Button
+            on:click={() => handleGoogleLogin()}
+            variant="raised"
+            color="secondary"
+            class="button-shaped-round">
+            <Label>Sign in</Label>
+          </Button>
+        {/if}
+      </div>
+    </Section>
+  </Row>
 </TopAppBar>
-
