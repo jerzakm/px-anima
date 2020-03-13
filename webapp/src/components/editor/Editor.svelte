bd46cf535570aa82092463119a6323d3cb649fb0
Marcin J <jerzakm@gmail.com>
2020-03-13 13:20:20 +0100

    basic layer and filter window

--- webapp/src/components/editor/Editor.svelte
+++ webapp/src/components/editor/Editor.svelte
@@ -1,7 +1,40 @@
 <script>
     import Canvas from './Canvas.svelte'
     import Filters from './Filters.svelte'
+    import Layers from './Layers.svelte'
 </script>
 
-<Canvas/>
-<Filters/>
+<!-- <Canvas/> -->
+
+<style>
+    .card{
+        background-color: #FFFFFF;
+        min-height: 50px;
+        width: 500px;
+        position: fixed;
+        top: 96px;
+        right:32px;
+        display: flex;
+        flex-direction: row;
+    }
+    .layers{
+        background-color: #CECECE;
+        width: 150px;
+    }
+    .filters{
+        background-color: #DDDDDD;
+        width:100%;
+    }
+</style>
+
+<div class="card">
+    <div class="layers">
+        <Layers/>
+    </div>
+    <div class="filters">
+        <Filters/>
+    </div>
+</div>
+
+
+<!-- <Filters/> -->
---
+++ webapp/src/components/editor/Layers.svelte
@@ -0,0 +1,51 @@
+<script>
+
+import { flip } from "svelte/animate";
+import { quintOut, quadIn, sineIn } from 'svelte/easing';
+import Sortable from "svelte-sortablejs";
+import * as Filters from 'pixi-filters'
+import { filterArray } from "../../stores";
+
+let list = [];
+let options = {
+  draggable: ".layers"
+};
+
+list.push({id: 0, name: 'Layer A'})
+list.push({id: 1, name: 'Layer B'})
+
+// $: list && filterArray.set(list) &&console.log('changed filter list')
+</script>
+
+<style>
+    .layer-preview {
+        width: 50px;
+        height: 50px;
+        background-color: white;
+    }
+    .layers{
+        list-style-type: none;
+    }
+    .layer-entry-container{
+        display:flex;
+        flex-direction: row;
+        align-items: center;
+        background-color: #DDDDDD;
+        margin: 6px;
+    }
+
+</style>
+
+<Sortable {options} bind:list>
+        {#each list as layer (layer.name)}
+          <li
+            animate:flip={{ duration: 70, easing: sineIn }}
+            sortable-id={layer.name}
+            class="layers">
+            <div class="layer-entry-container">
+                <div class="layer-preview"/>
+                <span>{layer.name}</span>
+            </div>
+          </li>
+        {/each}
+</Sortable>
